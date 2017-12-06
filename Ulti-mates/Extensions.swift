107596dccf2df10062cc317ece8a28afcd429655
travis ouellette <trouel15@uw.edu>
2017-12-06 13:01:10 -0800

    initial

--- Ulti-mates/Extensions.swift
+++ Ulti-mates/Extensions.swift
@@ -272,8 +272,8 @@ extension Double {
 }
 
 extension UIColor {
-	convenience init(r: Int, g: Int, b: Int, alpha: CGFloat) {
-		self.init(red: CGFloat(r)/255, green: CGFloat(g)/255, blue: CGFloat(b)/255, alpha: alpha)
+	convenience init(r: Int, g: Int, b: Int) {
+		self.init(red: CGFloat(r)/255, green: CGFloat(g)/255, blue: CGFloat(b)/255, alpha: 1)
 	}
 	
 	func lighten(_ saturationMultiplier: CGFloat = 0.5) -> UIColor {
